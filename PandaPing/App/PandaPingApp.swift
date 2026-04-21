7017ff9470c3a61fe49e74e4ec7967fb3ea68629
Vladimir Kolev <vladimir.r.kolev@gmail.com>
2026-04-21 13:59:46 +0200

    feat: Restructure of the project and prepare for the development

---
+++ PandaPing/App/PandaPingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PandaPingApp.swift
+//  PandaPing
+//
+//  Created by Vladimir Kolev on 21.04.26.
+//
+
+import SwiftUI
+
+@main
+struct PandaPingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
